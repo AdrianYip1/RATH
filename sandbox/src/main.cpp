d0935b3a36cb93d37291cbf2252826e535e6c456
Adrian Yip <yipadrian1119@gmail.com>
2026-08-02 16:36:56 -0600

    comments: comments on the sandbox main.cpp

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 
 int main() {
+// Detects the platform RATH is being ran on
 #if defined(RATH_PLATFORM_WINDOWS)
 	const char* platform = "Windows";
 #elif defined(RATH_PLATFORM_LINUX)
@@ -13,11 +14,14 @@ int main() {
 #else
 	const char* platform = "unknown";
 #endif
-
+	
+	// Init application with dimensions and name
 	Rath::Application app(1600, 1200, "RATH");
 	try {
+		// Start render loop
 		app.run();
 	}
+	// Catches any exceptions and runtime errors
 	catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
